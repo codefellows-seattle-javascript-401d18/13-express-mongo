6311d9d4fe7f480f0f358bcbe92b7a6baa2177f0
kpryzk <kepryzk@gmail.com>
2017-09-06 12:55:16 -0700

    init commit

---
+++ lab-katherine/lib/server.js
@@ -0,0 +1,37 @@
+
+
+'use strict'
+
+const debug = require('debug')('http:server')
+
+// express setup
+const express = require('express')
+const router = express.Router()
+const app = module.exports = express()
+
+//mongoose setup
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/toy/dev'
+mongoose.Promise = require('bluebird')
+mongoose.connect(MONGODB_URI, {useMongoClient: true})
+
+// middleware
+const bodyParser = require('body-parser').json()
+const cors = require('./cors')
+const errorMiddleware = require('./error-middleware')
+
+// routes (kinda middleware too)
+require('../route/route-toy')(router)
+// require('./route/route-kid')(router)
+// require('./route/route-family')(router)
+
+// mount middleware
+app.use(bodyParser)
+app.use(cors)
+app.use(router)
+// this should always be last to catch any errors within the callback chain
+app.use(errorMiddleware)
+
+app.all('/*', (req, res) => res.sendStatus(404))
+
+module.exports = app
