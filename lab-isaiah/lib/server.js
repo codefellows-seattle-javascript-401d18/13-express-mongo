d5d9d4c172d8dc6da3b63624070afb9a031fed0c
lonewalker72 <walkisai.72@gmail.com>
2017-09-06 22:40:29 -0700

    Complete scaffolding/mongod and mongoose installation. Working on getting mongod up and running

---
+++ lab-isaiah/lib/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// const debug = require('debug')('http:server');
+
+// express setup
+const express = require('express');
+const router = express.Router();
+const app = express();
+
+// mongoose setup
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/toy-dev';
+mongoose.Promise = require('bluebird');
+mongoose.connect(MONGODB_URI, {useMongoClient: true});
+
+// middleware
+const bodyParser = require('body-parser').json();
+const cors = require('./cors');
+const errorMiddleware = require('./error-middleware');
+
+// routes (middleware)
+require('../route/route-toy')(router);
+
+// mount middleware
+app.use(bodyParser);
+app.use(cors);
+app.use(router);
+app.use(errorMiddleware);
+
+app.all('/*', (req, res) => res.sendStatus(404));
+
+module.exports = app;
