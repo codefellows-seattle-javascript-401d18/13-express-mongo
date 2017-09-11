a46f113f747a8f30148bb549b257860b0475a3b4
esack7 <isaacheist@gmail.com>
2017-09-11 08:21:16 -0700

    made corrections on routes

---
+++ lab-isaac/README.md
@@ -0,0 +1,13 @@
+# Lab 13: Express and MongoDB
+
+## About
+
+This lab assignment is a Nodejs backend API app.  It takes http POST, GET, PUT, and DELETE calls.  The database is MongoDB for the toys being tracked.  The main urls is ```api/toys```.  Jest is used for testing the toy POST, GET, PUT, and DELETE calls.
+
+## Installation
+
+To install this app, you need to have Nodejs installed on your system.  MongoDB needs to also be installed and running on you system.  Clone this repo and navigate to ```13-express-mongo/lab-isaac``` and then ```npm install``` will install all necessary packages via npm.
+
+## Run Application
+
+To run this application you have the following 3 options:  ```npm run start```, ```npm run start:watch```, or ```npm run start:debug```.
--- lab-isaac/lib/error-middleware.js
+++ lab-isaac/lib/error-middleware.js
@@ -3,16 +3,33 @@
 const createError = require('http-errors');
 const debug = require('debug')('http:error-middleware');
 
-module.exports = function(err, req, res, next) {
+module.exports = function(err, req, res) {
   if(err.status) {
-    debug('user error');
-    res.status(err.status).send(err.name);
-    next();
+    switch (true) {
+    case err.message.toLowerCase().includes('validation failed'):
+      debug('validation failed');
+      res.status(400).send(err.name);
+      break;
+
+    case err.message.toLowerCase().includes('duplicate key'):
+      debug('duplicate key');
+      res.status(409).send(err.name);
+      break;
+
+    case err.message.toLowerCase().includes('objectId failed'):
+      debug('objectId failed');
+      res.status(404).send(err.name);
+      break;
+
+    default:
+      debug('default err status');
+      res.status(err.status).send(err.name);
+      break;
+    }
     return;
   }
   debug('server-error');
   err = createError(500, err.message);
   res.status(err.status).send(err.name);
-  next();
   // implicit return
 };
