802d2daff6c0057c15735d69a2243c8725b3f33c
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-09-11 18:35:34 -0700

    Put is working!

--- lab-michelle/__test__/route-toy.test.js
+++ lab-michelle/__test__/route-toy.test.js
@@ -35,108 +35,126 @@ describe('Testing toy routes', function() {
               expect(err.status).toBe(500);
             });
         });
-        describe('GET reqs', () => {
-          describe('Valid reqs', () => {
-            test('should get a toy given a valid request', done => {
-              superagent.get(`:3000/api/toy/${this.mockToy._id}`)
-                .type('application/json')
-                .then(res => {
-                  this.res = res;
-                  expect(res.body).toHaveProperty('name');
-                  expect(res.body).toHaveProperty('desc');
-                  expect(res.status).toBe(200);
-                  done();
-                });
-            });
-          });
-          describe('Invalid reqs', () => {
-            test('should not get a valid thing', () => {
-              return superagent.get(`:3000/api/toy/2462456`)
-                .catch(res => {
-                  expect(res.status).toBe(500);
-                });
+
+
+
+
+      });
+      describe('GET reqs', () => {
+        describe('Valid reqs', () => {
+          test('should get a toy given a valid request', done => {
+            superagent.get(`:3000/api/toy/${this.mockToy._id}`)
+            .type('application/json')
+            .then(res => {
+              this.res = res;
+              expect(res.body).toHaveProperty('name');
+              expect(res.body).toHaveProperty('desc');
+              expect(res.status).toBe(200);
+              done();
             });
           });
         });
-        describe('GET all the items', () => {
-          describe('Valid reqs', () => {
-            test('should get an array of toys given a valid req', done => {
-              superagent.get(`:3000/api/toy`)
-                .type('application/json')
-                .then(res => {
-                  this.res = res;
-                  expect(res.body).toBeInstanceOf(Array);
-                  done();
-                });
-            });
-          });
-          describe('Invalid reqs', () => {
-            test('should not get a valid thing', function() {
-              return superagent.get(`:3000/api/toy/1324235`)
-                .catch(res => {
-                  expect(res.status).toBe(500);
-                });
-            });
-          });
-        });
-        // describe('PUT reqs', function() {
-        //   beforeAll(() => {
-        //     return superagent.post(`:3000/api/toy`)
-        //       .send({name:'Moana', desc: 'sailor lady'})
-        //       .then(res => {
-        //         this.resPost = res;
-        //       });
-        //   });
-        //   afterAll(() => {
-        //     return Promise.all([
-        //       Toy.remove()
-        //     ]);
-        //   });
-        //   describe('Valid reqs', () => {
-        //     test('should return a status of 204 No Content', ()=> {
-        //       return superagent.put(`:3000/api/toy/${this.resPost.body._id}`)
-        //         .send({name: 'Moana', desc:'sailor lady'})
-        //         .then(res => {
-        //           expect(res.status).toBe(204);
-        //         });
-        //     });
-        //     test('should update the existing record in the DB', () => {
-        //       return superagent.get(`:3000/api/toy/${this.resPost.body._id}`)
-        //         .then(res => {
-        //           expect(res.body.name).toBe('Moana');
-        //           expect(res.body.desc).toBe('sailor lady');
-        //         });
-        //     });
-        //   });
-        //   describe('Invalid reqs', () => {
-        //     //test here
-        //   });
-        // });
-        describe('DELETE reqs', ()=> {
-          describe('Valid reqs', ()=> {
-            beforeAll( done => {
-              superagent.delete(`:3000/api/toy/${thinrs.mockToy._id}`)
-                .then(res => {
-                  this.resDelete = res;
-                  done();
-                });
-            });
-            test('should return a 204 No Content', () => {
-              expect(this.res.status).toBe(204);
-            });
-          });
-          describe('Invalid reqs', ()=> {
-            test('should return 404', done => {
-              superagent.delete(':3000/api/toy')
-                .query({_id: 'abionnarog'})
-                .catch(res => {
-                  expect(res.status).toBe(500);
-                  done();
-                });
+        describe('Invalid reqs', () => {
+          test('should not get a valid thing', () => {
+            return superagent.get(`:3000/api/toy/2462456`)
+            .catch(res => {
+              expect(res.status).toBe(500);
             });
           });
         });
       });
+
+      describe('GET all the items', () => {
+        describe('Valid reqs', () => {
+          test('should get an array of toys given a valid req', done => {
+            superagent.get(`:3000/api/toy`)
+            .type('application/json')
+            .then(res => {
+              this.res = res;
+              expect(res.body).toBeInstanceOf(Array);
+              done();
+            });
+          });
+        });
+        describe('Invalid reqs', () => {
+          test('should not get a valid thing', function() {
+            return superagent.get(`:3000/api/toy/1324235`)
+            .catch(res => {
+              expect(res.status).toBe(500);
+            });
+          });
+        });
       });
-});
+    });
+    describe('PUT reqs', function() {
+      beforeAll(() => {
+        return superagent.post(`:3000/api/toy`)
+        .send({name:'Moana', desc: 'sailor lady'})
+        .then(res => {
+          this.resPost = res;
+          console.log(res.body);
+        });
+      });
+      afterAll(() => {
+        return Promise.all([
+          Toy.remove(),
+        ]);
+      });
+      describe('Valid reqs', () => {
+        test('should return a status of 204 No Content', done=> {
+          console.log(this.resPost.body._id);
+          superagent.put(`:3000/api/toy/${this.resPost.body._id}`)
+          // .type('application/json')
+          .send({name: 'Moana', desc:'cat person'})
+          .then(res => {
+            console.log(res);
+            expect(res.status).toBe(204);
+            done();
+          });
+        });
+        test('should update the existing record in the DB', () => {
+          return superagent.get(`:3000/api/toy/${this.resPost.body._id}`)
+          .then(res => {
+            expect(res.body.name).toBe('Moana');
+            expect(res.body.desc).toBe('cat person');
+          });
+        });
+      });
+      describe('Invalid reqs', () => {
+        test('should return a 500 error', done => {
+          superagent.put(':3000/api/toy')
+          .type('application/json')
+          .send({})
+          .catch(err => {
+            expect(err.status).toBe(400);
+          })
+          done();
+        });
+      });
+    });
+    describe('DELETE reqs', ()=> {
+      describe('Valid reqs', ()=> {
+        beforeAll( done => {
+          superagent.delete(`:3000/api/toy/${this.mockToy._id}`)
+          .then(res => {
+            this.resDelete = res;
+            done();
+          });
+        });
+        test('should return a 204 No Content', () => {
+          expect(res.status).toBe(204);
+        });
+      });
+      describe('Invalid reqs', ()=> {
+        test('should return 404', done => {
+          superagent.delete(':3000/api/toy')
+          .query({_id: 'abionnarog'})
+          .catch(res => {
+            expect(res.status).toBe(500);
+            done();
+          });
+        });
+      });
+    });
+  });
 });
--- lab-michelle/route/route-toy.js
+++ lab-michelle/route/route-toy.js
@@ -31,13 +31,13 @@ module.exports = function (router) {
       .catch(next);
   });
 
-  router.put('/api/toy', (req, res) => {
+  router.put('/api/toy/:_id', (req, res) => {
     return Toy.findByIdAndUpdate(req.params._id, req.body, {upsert: true, runValidators: true})
-      .then(() => res.status(204))
-      .catch(next);
+      .then(() => res.sendStatus(204))
+      .catch(err => console.log(err.message));
   });
 
-  router.delete('/api/toy', (req, res) => {
+  router.delete('/api/toy/:_id', (req, res, next) => {
     debug('/api/data DELETE');
     return Toy.findByIdAndRemove(req.params._id)
       .then(() => res.status(204))
